5cba6242097012e830855c977744d125c7d17ec8
kagos <kristin.agos@yahoo.com>
2017-01-29 16:33:27 -0600

    Mocha-Chai tested; started basic Dashboard TDD

---
+++ test/dashboard.js
@@ -0,0 +1,11 @@
+var expect    = require("chai").expect;
+var converter = require("../app/dashboard");
+
+var expect    = require("chai").expect;
+var converter = require("../app/dashboard");
+
+describe("Dashboard", function() {
+	it("it checks for user id", function() {
+		
+	});
+});
